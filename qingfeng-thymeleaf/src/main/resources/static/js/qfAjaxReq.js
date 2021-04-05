451fb049f7a10e8bea70353f92e3b192711a2039
Administrator <axtaxt@163.com>
2021-04-05 17:36:31 +0800

    bug修复

--- qingfeng-thymeleaf/src/main/resources/static/js/qfAjaxReq.js
+++ qingfeng-thymeleaf/src/main/resources/static/js/qfAjaxReq.js
@@ -1,12 +1,13 @@
 /**
  * Created by anxingtao on 2019-3-13.
  */
+var prefix = $("#ctxValue").val();
 //查询省份
 function findAddress(parent_id,show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"system/area/findList?parent_id="+parent_id,//url
+        url: prefix+"/system/area/findList?parent_id="+parent_id,//url
         data: "",
         success: function (res) {
             if (res.success) {
@@ -40,13 +41,13 @@ function findSelectDictionary(parent_id,show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"system/dictionary/findList?parent_id="+parent_id,//url
+        url: prefix+"/system/dictionary/findList?parent_id="+parent_id,//url
         data: "",
         success: function (res) {
             if (res.success) {
                 var tt = '<option value="">请选择</option>';
                 $.each(res.data,function(i,n){
-                    if(value== n.id){
+                    if(value== n.id || value== n.code){
                         tt += '<option value="'+ n.id+'" selected>'+ n.name+'</option>';
                     }else{
                         tt += '<option value="'+ n.id+'">'+ n.name+'</option>';
@@ -70,17 +71,17 @@ function findSelectDictionary(parent_id,show_id,value){
 
 
 //查询Checkbox字典项
-function findCheckboxDictionary(parent_code,show_ctx,show_id,value){
+function findCheckboxDictionary(parent_code,show_prefix,show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"system/dictionary/findList?parent_code="+parent_code,//url
+        url: prefix+"/system/dictionary/findList?parent_code="+parent_code,//url
         data: "",
         success: function (res) {
             if (res.success) {
                 var tt = '';
                 $.each(res.data,function(i,n){
-                    if(value.indexOf(n.id)!=-1){
+                    if(value.indexOf(n.id)!=-1||value.indexOf(n.code)!=-1){
                         tt += '<input type="checkbox" checked name="'+show_prefix+show_id+'"  lay-filter="checkField" value="'+ n.id+'" lay-skin="primary" title="'+ n.name+'">';
                     }else{
                         tt += '<input type="checkbox" name="'+show_prefix+show_id+'"  lay-filter="checkField" value="'+ n.id+'" lay-skin="primary" title="'+ n.name+'">';
@@ -110,13 +111,13 @@ function findRadioDictionary(parent_code,show_prefix,show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"system/dictionary/findList?parent_code="+parent_code,//url
+        url: prefix+"/system/dictionary/findList?parent_code="+parent_code,//url
         data: "",
         success: function (res) {
             if (res.success) {
                 var tt = '';
                 $.each(res.data,function(i,n){
-                    if(value== n.id){
+                    if(value== n.id || value== n.code){
                         tt += '<input type="radio" checked name="'+child_prefix+show_id+'" value="'+ n.id+'" title="'+ n.name+'">';
                     }else{
                         tt += '<input type="radio" name="'+child_prefix+show_id+'" value="'+ n.id+'" title="'+ n.name+'">';
@@ -141,14 +142,14 @@ function findValueDictionary(show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"system/dictionary/findInfoJson?id="+value,//url
+        url: prefix+"/system/dictionary/findInfoJson?id="+value,//url
         data: "",
         success: function (res) {
             if (res.success) {
                 var names = [];
                 $.each(res.data,function(i,n){
                     names.push(n.name);
-                })
+                });
                 $("#"+show_id).html(names.join(','));
                 form.render();
             }else{
@@ -168,7 +169,7 @@ function findTabClassify(show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"customize/tclassify/findList?status=Y",//url
+        url: prefix+"/customize/tclassify/findList?status=Y",//url
         data: "",
         success: function (res) {
             if (res.success) {
@@ -202,7 +203,7 @@ function findTableList(classify_id,show_id,value){
     $.ajax({
         type: "GET",//方法类型
         dataType: "json",//预期服务器返回的数据类型
-        url: ctx+"customize/ttable/findList?status=Y&classify_id="+classify_id,//url
+        url: prefix+"/customize/ttable/findList?status=Y&classify_id="+classify_id,//url
         data: "",
         success: function (res) {
             if (res.success) {
